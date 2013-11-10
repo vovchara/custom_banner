8f8703ee15f4362ab28d32c2b553e090600074e9
Volodymyr Martyniuk <vovchara@ukr.net>
2013-11-10 13:22:09 +0200

    added if then in answer to mail.php

--- src/res/php/mail.php
+++ src/res/php/mail.php
@@ -4,6 +4,10 @@
     $message = $_POST["message"];
     $from = "vovchara913@gmail.com";
     $headers = "From: $from";
-    mail($to,$subject,$message,$headers);
-    echo "Successfully sent";
+    if (mail($to,$subject,$message,$headers)){
+       echo "Successfully sent";
+    }
+    else{
+       echo "sending error";
+    }
 ?>
